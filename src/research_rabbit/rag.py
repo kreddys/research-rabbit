6343dc95226b5ed858aecfa80bee6cbe66b8d8df
kreddy <kishore5214@outlook.com>
2024-12-31 13:33:16 -0600

    rag wip

--- src/research_rabbit/rag.py
+++ src/research_rabbit/rag.py
@@ -1,9 +1,9 @@
 # /src/research_rabbit/rag.py
 
-from langchain.document_loaders import DirectoryLoader
+from langchain_community.document_loaders import DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import Chroma
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
 import os
 
 class RAGManager:
